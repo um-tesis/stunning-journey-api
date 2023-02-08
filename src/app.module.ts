48964a7411bf09c2a3a9ade3bed4100f16363e65
Jose Algorta <jpalgort@gmail.com>
2023-02-07 23:36:34 -0300

    Add graphql snadbox instead of playground.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { UsersModule } from './api/users/users.module';
 import { OrganizationsModule } from './api/organizations/organizations.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
   imports: [
@@ -14,6 +15,8 @@ import { OrganizationsModule } from './api/organizations/organizations.module';
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     PrismaModule,
     UsersModule,
