79a8a408d22022b8a9e7edbbe7514ec2efda3a31
Jose Algorta <jpalgort@gmail.com>
2023-02-08 00:02:27 -0300

    Configure ESLint and husky

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path';
+
+import { OrganizationsModule } from './api/organizations/organizations.module';
+import { UsersModule } from './api/users/users.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
-import { UsersModule } from './api/users/users.module';
-import { OrganizationsModule } from './api/organizations/organizations.module';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 @Module({
   imports: [
