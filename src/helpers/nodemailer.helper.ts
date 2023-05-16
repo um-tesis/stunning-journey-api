8e8400123915ebe2bbab75f12494ce78f9520734
Jose Algorta <jpalgort@gmail.com>
2023-05-16 00:35:43 -0300

    Small fix

--- src/helpers/nodemailer.helper.ts
+++ src/helpers/nodemailer.helper.ts
@@ -1,5 +1,5 @@
 import * as nodemailer from 'nodemailer';
-import config from 'src/config/config';
+import config from 'src/api/config';
 
 const { NODEMAILER_PASSWORD, LIBERA_EMAIL_ACCOUNT } = config;
 
